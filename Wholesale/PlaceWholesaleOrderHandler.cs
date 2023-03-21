5d78f7284a9046c9013a4263b9d6f919ab86b1a7
Konstantin Lepeshenkov <5447190+scale-tone@users.noreply.github.com>
2023-03-21 17:36:56 +0100

    devcontainer

--- Wholesale/PlaceWholesaleOrderHandler.cs
+++ Wholesale/PlaceWholesaleOrderHandler.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Messages;
 using NServiceBus;
 using NServiceBus.Logging;
@@ -12,6 +13,12 @@ namespace Wholesale
 
         public Task Handle(PlaceWholesaleOrder message, IMessageHandlerContext context)
         {
+            // To demonstrate that order is preserved even if a message gets retried
+            if (Random.Shared.Next() % 2 == 0)
+            {
+                throw new Exception("Oops");
+            }
+
             log.Info($"Wholesale has received PlaceWholesaleOrder for customer {message.CustomerId} with Id {message.Id}");
             return Task.CompletedTask;
         }
