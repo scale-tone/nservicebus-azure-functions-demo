3955b6d29af7d816d338cf594bef8ea618f169ec
Konstantin Lepeshenkov <5447190+scale-tone@users.noreply.github.com>
2023-03-21 20:32:02 +0100

    Update TestDeduplicationHandler.cs

--- ClientUI/TestDeduplicationHandler.cs
+++ ClientUI/TestDeduplicationHandler.cs
@@ -16,11 +16,13 @@ namespace ClientUI
             log.Info($"Received command with id {message.OrderId}");
 
             var sendOptions = new SendOptions();
+            // This queue should be created with 'Enable duplicate detection' flag set
             sendOptions.SetDestination("dedup");
 
             // Explicitly setting native message's MessageId to enable deduplication
             sendOptions.CustomizeNativeMessage(m => m.MessageId = message.OrderId);
 
+            // Sending two messages, but expect only one to appear
             await context.Send(message, sendOptions);
             await context.Send(message, sendOptions);
         }
